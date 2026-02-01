d6f8abc12ef947baf547b8df7e6fd7120c713c6f
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-01 19:39:36 +0100

    06_liste_potions

--- 1_Basics/exercices/06_liste_potions.js
+++ 1_Basics/exercices/06_liste_potions.js
@@ -1,1 +1,10 @@
 // Liste des potions
+let List_Potion = ["Filtre d'amour", "Potion d'invisibilité", "Elixir de jeunesse"]
+console.log(List_Potion)
+// console.log(List_Potion[0])
+// console.log(List_Potion[1])
+// console.log(List_Potion[2])
+
+// for (let potion of List_Potion.values()) {
+//     console.log(potion)
+// }
