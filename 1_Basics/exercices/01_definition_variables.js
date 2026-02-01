cecdc4f523d8d5fe8b564792ab223fec2753fff9
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-01 17:44:32 +0100

    01_definition_variables

--- 1_Basics/exercices/01_definition_variables.js
+++ 1_Basics/exercices/01_definition_variables.js
@@ -1,1 +1,12 @@
 // Définition de variables
+
+const money = "🪙"
+const name_store = "Potion Store";
+let stock_potion_care = 10;
+let price_potion_care = 2;
+let store_is_open = true;
+
+console.log("Ma boutique se nomme " + name_store);
+console.log("Nous avons " + stock_potion_care + " potions de type soin en stock");
+console.log("Le tarif des potions de type soin est de " + price_potion_care + " " + money);
+console.log("Boutique ouverte : " + store_is_open);
