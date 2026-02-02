edc86f5cf14164e545a90993142748cde703f041
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-02 13:20:09 +0100

    07_affichage_potions

--- 1_Basics/exercices/07_affichage_potions.js
+++ 1_Basics/exercices/07_affichage_potions.js
@@ -1,4 +1,17 @@
-// Affichage des potions
-
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+// Affichage des potions
+
+// Première potion
+console.log(potions[0])
+
+// Dernière potion
+console.log(potions[potions.length - 1])
+
+// Avec boucle for
+for (potion of potions) {
+    console.log(`Nous avons de la ${potion}`)
+}
+
+
