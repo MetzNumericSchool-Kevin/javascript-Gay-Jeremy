bb3ca4416ab5d29fbf1fd5c0e3183158bfe53cd9
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-02 16:36:08 +0100

    11_affichage_infos_potion

--- 1_Basics/exercices/11_affichage_infos_potion.js
+++ 1_Basics/exercices/11_affichage_infos_potion.js
@@ -1,8 +1,10 @@
-// Affichons les informations de la potion
-
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potion_soin = {
   nom: "Potion de soin",
   prix: 50,
   stock: 10,
-};
+}
+
+// Affichons les informations de la potion
+console.log(`Le nom de la potion est ${potion_soin.nom}`)
+console.log(`Le prix de la potion est ${potion_soin['prix']}`)
