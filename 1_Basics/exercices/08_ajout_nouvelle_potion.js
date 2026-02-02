a602a290b236088c24909e74890dd3e146048235
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-02 13:32:22 +0100

    08_ajout_nouvelle_potion

--- 1_Basics/exercices/08_ajout_nouvelle_potion.js
+++ 1_Basics/exercices/08_ajout_nouvelle_potion.js
@@ -1,4 +1,9 @@
-// Ajout d'une nouvelle potion
-
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+// Ajout d'une nouvelle potion
+
+potions.push("Filtre d'amour")
+console.log(potions)
+potions.pop()
+console.log(potions)
