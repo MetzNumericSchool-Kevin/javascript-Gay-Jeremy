dd8087ea4beb5451f914b3e588c3be1dd6031eaf
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-03 08:41:50 +0100

    12_affichage_inventaire

--- 1_Basics/exercices/12_affichage_inventaire.js
+++ 1_Basics/exercices/12_affichage_inventaire.js
@@ -1,4 +1,30 @@
-// Aventurier, regarde tout ce que je vends !
-
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const inventaire = [];
+
+const potion_soin = {
+    nom: "Potion de soin",
+    prix: 50,
+    stock: 10,
+}
+
+const potion_mana = {
+    nom:"Potion de mana",
+    prix:100,
+    stock:3
+}
+
+const potion_endurance = {
+    nom:"Potion d'endurance",
+    prix:20,
+    stock:10
+}
+
+// Aventurier, regarde tout ce que je vends !
+inventaire.push(potion_soin,potion_mana,potion_soin)
+console.log(inventaire)
+for (potion of inventaire) {
+    console.log(`Nom : ${potion.nom}`
+                `Prix : ${potion.prix}`
+                `Stock : ${potion.stock}`
+    )
+}
