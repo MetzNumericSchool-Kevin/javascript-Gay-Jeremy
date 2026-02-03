19b81a7648369c7688d83e18bb2021b9b3fffae5
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-02-03 15:19:41 +0100

    16_tri_simple

--- 1_Basics/exercices/16_tri_simple.js
+++ 1_Basics/exercices/16_tri_simple.js
@@ -1,5 +1,3 @@
-// Tri simple
-
 // Inventaire de la boutique (non trié)
 const inventaire = [
   { nom: "Potion de force", prix: 60, stock: 3 },
@@ -7,3 +5,21 @@ const inventaire = [
   { nom: "Potion de soin", prix: 50, stock: 10 },
   { nom: "Potion d'endurance", prix: 40, stock: 7 }
 ];
+
+// Tri simple
+
+for (let i = 0; i < inventaire.length - 1; i++) {
+  for (let j = 0; j < inventaire.length - i - 1; j++) {
+
+    if (inventaire[j].prix > inventaire[j + 1].prix) {
+
+      let temp = inventaire[j];
+      inventaire[j] = inventaire[j + 1];
+      inventaire[j + 1] = temp;
+
+    }
+  }
+}
+
+console.log(inventaire)
+
